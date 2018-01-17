47c7cd4a6dbc2bbdd20995c1e08b3d3d1f733c47
2vg <m0fqn090310@gmail.com>
2018-01-17 21:53:52 +0900

    add OPTIONS router for CORS, add Content-Length check (Issue buf cant free.)

--- middleware/mofuw_router.nim
+++ middleware/mofuw_router.nim
@@ -1,9 +1,12 @@
-import ../mofuw
+import mofuw
+import lib/httputils
+import strutils
 
 type
   router = ref object
     GET: seq[router_t]
     POST: seq[router_t]
+    OPTIONS: seq[router_t]
 
   router_t = object
     path: string
@@ -12,7 +15,8 @@ type
 proc newMofuwRouter*(): router =
   result = router(
     GET: @[],
-    POST: @[]
+    POST: @[],
+    OPTIONS: @[]
   )
 
 proc mofuwGET*(r: router, path: string, cb: proc(req: ptr mofuwReq, res: ptr mofuwRes)) =
@@ -20,6 +24,9 @@ proc mofuwGET*(r: router, path: string, cb: proc(req: ptr mofuwReq, res: ptr mof
 
 proc mofuwPOST*(r: router, path: string, cb: proc(req: ptr mofuwReq, res: ptr mofuwRes)) =
   r.POST.add(router_t(path: path, cb: cb))
+
+proc mofuwOPTIONS*(r: router, path: string, cb: proc(req: ptr mofuwReq, res: ptr mofuwRes)) =
+  r.OPTIONS.add(router_t(path: path, cb: cb))
 
 proc mofuwRouting*(r: router, request: ptr mofuwReq, response: ptr mofuwRes) {.inline.}=
   case getMethod(request)
@@ -39,6 +46,23 @@ proc mofuwRouting*(r: router, request: ptr mofuwReq, response: ptr mofuwRes) {.i
     else:
       block searchRoute:
         for value in r.POST:
+          if getPath(request) == value.path:
+            for v in request.reqHeader:
+              if not (($(v.name))[0 .. v.namelen] == "Content-Length"):
+                continue
+              else:
+                request.reqBodyLen = parseInt(($(v.value))[0 .. v.valuelen])
+                request.reqBody = ($(request.reqBody))[0 .. request.reqBodyLen - 1]
+                value.cb(request, response)
+                break searchRoute
+              response.mofuw_send(badRequest())
+        notFound(response)
+  of "OPTIONS":
+    if r.OPTIONS.len == 0:
+      notFound(response)
+    else:
+      block searchRoute:
+        for value in r.OPTIONS:
           if getPath(request) == value.path:
             value.cb(request, response)
             break searchRoute
