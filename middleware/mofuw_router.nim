9293f6d3d0f82ff1da664deb12335528d56d546d
2vg <m0fqn090310@gmail.com>
2018-01-20 12:51:49 +0900

    add PUT and DELETE routing ✨

--- middleware/mofuw_router.nim
+++ middleware/mofuw_router.nim
@@ -6,6 +6,8 @@ type
   router = ref object
     GET: seq[router_t]
     POST: seq[router_t]
+    PUT: seq[router_t]
+    DELETE: seq[router_t]
     OPTIONS: seq[router_t]
 
   router_t = object
@@ -16,6 +18,8 @@ proc newMofuwRouter*(): router =
   result = router(
     GET: @[],
     POST: @[],
+    PUT: @[],
+    DELETE: @[],
     OPTIONS: @[]
   )
 
@@ -24,6 +28,12 @@ proc mofuwGET*(r: router, path: string, cb: proc(req: ptr mofuwReq, res: ptr mof
 
 proc mofuwPOST*(r: router, path: string, cb: proc(req: ptr mofuwReq, res: ptr mofuwRes)) =
   r.POST.add(router_t(path: path, cb: cb))
+
+proc mofuwPUT*(r: router, path: string, cb: proc(req: ptr mofuwReq, res: ptr mofuwRes)) =
+  r.PUT.add(router_t(path: path, cb: cb))
+  
+proc mofuwDELETE*(r: router, path: string, cb: proc(req: ptr mofuwReq, res: ptr mofuwRes)) =
+  r.DELETE.add(router_t(path: path, cb: cb))
 
 proc mofuwOPTIONS*(r: router, path: string, cb: proc(req: ptr mofuwReq, res: ptr mofuwRes)) =
   r.OPTIONS.add(router_t(path: path, cb: cb))
@@ -48,7 +58,47 @@ proc mofuwRouting*(r: router, request: ptr mofuwReq, response: ptr mofuwRes) {.i
         for value in r.POST:
           if getPath(request) == value.path:
             for v in request.reqHeader:
-              if not (($(v.name))[0 .. v.namelen] == "Content-Length"):
+              if v.namelen == 0: break
+
+              if not(($(v.name))[0 .. v.namelen] == "Content-Length"):
+                continue
+              else:
+                request.reqBodyLen = parseInt(($(v.value))[0 .. v.valuelen])
+                request.reqBody = ($(request.reqBody))[0 .. request.reqBodyLen - 1]
+                value.cb(request, response)
+                break searchRoute
+              response.mofuw_send(badRequest())
+        notFound(response)
+  of "PUT":
+    if r.POST.len == 0:
+      notFound(response)
+    else:
+      block searchRoute:
+        for value in r.POST:
+          if getPath(request) == value.path:
+            for v in request.reqHeader:
+              if v.namelen == 0: break
+
+              if not(($(v.name))[0 .. v.namelen] == "Content-Length"):
+                continue
+              else:
+                request.reqBodyLen = parseInt(($(v.value))[0 .. v.valuelen])
+                request.reqBody = ($(request.reqBody))[0 .. request.reqBodyLen - 1]
+                value.cb(request, response)
+                break searchRoute
+              response.mofuw_send(badRequest())
+        notFound(response)
+  of "DELETE":
+    if r.POST.len == 0:
+      notFound(response)
+    else:
+      block searchRoute:
+        for value in r.POST:
+          if getPath(request) == value.path:
+            for v in request.reqHeader:
+              if v.namelen == 0: break
+
+              if not(($(v.name))[0 .. v.namelen] == "Content-Length"):
                 continue
               else:
                 request.reqBodyLen = parseInt(($(v.value))[0 .. v.valuelen])
