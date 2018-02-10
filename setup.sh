de5c5e9b494db7aea02099a62cadc91aeaca871e
2vg <m0fqn090310@gmail.com>
2018-02-10 21:36:39 +0900

    add: Nim devel and libuv 1.19 setup script.

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+wget https://dist.libuv.org/dist/v1.19.1/libuv-v1.19.1.tar.gz
+
+tar xf libuv-v1.19.1.tar.gz
+
+cd libuv-v1.19.1
+
+sh autogen.sh
+
+./configure
+
+make
+
+make install
+
+cd ../
+
+git clone -b devel https://github.com/nim-lang/Nim.git nim
+
+cd nim
+
+git clone --depth 1 https://github.com/nim-lang/csources.git
+
+cd csources
+
+sh build.sh
+
+cd ../
+
+bin/nim c koch
+
+./koch boot -d:release
+
+./koch nimble
+
+./koch tools
