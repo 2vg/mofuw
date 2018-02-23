fa94227ac8e6fc65886dc2161632cfbcbfdb52d6
2vg <m0fqn090310@gmail.com>
2018-02-23 18:22:44 +0900

    add: add routing macro but not complete yet

--- asyncdispatch_test/mofuw.nim
+++ asyncdispatch_test/mofuw.nim
@@ -1,16 +1,24 @@
-import asyncdispatch, net, nativesockets, threadpool, osproc, strtabs
+import
+  net,
+  hashes,
+  macros,
+  osproc,
+  tables,
+  strtabs,
+  strutils,
+  threadpool,
+  asyncdispatch,
+  nativesockets
 
 when defined(windows):
-  from winlean import TCP_NODELAY, WSAEWOULDBLOCK
-
-  const
-    EAGAIN = WSAEWOULDBLOCK
-    EWOULDBLOCK = WSAEWOULDBLOCK
+  from winlean import TCP_NODELAY
 else:
   from posix import TCP_NODELAY
 
-import lib/httputils
-import lib/mofuparser
+import
+  lib/httputils,
+  lib/mofuparser,
+  lib/objectPool
 
 export asyncdispatch
 export httputils
@@ -18,17 +26,16 @@ export httputils
 type
   mofuwReq* = ref object
     reqLine: HttpReq
-    reqHeader*: array[48, headers]
-    reqHeaderAddr: ptr array[48, headers]
+    reqHeader*: array[16, headers]
+    reqHeaderAddr: ptr array[16, headers]
     reqBody*: string
-    reqBodyLen*: int
+    reqBodyLen: int
     params*: StringTableRef
     tmp*: cstring
-  
+
   mofuwRes* = ref object
     fd: AsyncFD
-    req: mofuwReq
-  
+
   Callback* = proc(req: mofuwReq, res: mofuwRes): Future[void]
 
 const
@@ -91,10 +98,11 @@ proc handler(fd: AsyncFD) {.async.} =
       break
     else:
       var
+        buf = ""
         request = mofuwReq(reqBody: "")
         response = mofuwRes()
 
-      request.reqBody.add(recv)
+      buf.add(recv)
       request.reqHeaderAddr = request.reqHeader.addr
 
       response.fd = fd
@@ -106,13 +114,16 @@ proc handler(fd: AsyncFD) {.async.} =
           if recv == "":
             break
 
-          request.reqBody.add(recv)
+          buf.add(recv)
 
-      let r = mp_req(addr(request.reqBody[0]), request.reqLine, request.reqHeaderAddr)
+      let r = mp_req(addr(buf[0]), request.reqLine, request.reqHeaderAddr)
 
       if r <= 0:
         await response.mofuwSend(notFound())
-        request.reqBody.setLen(0)
+        buf.setLen(0)
+
+      shallowcopy(request.reqBody, buf[r .. buf.len - 1])
+      request.reqBodyLen = request.reqBody.len
 
       await callback(request, response)
 
@@ -152,3 +163,87 @@ proc mofuwRun*(port: int = 8080, backlog: int = SOMAXCONN,
     spawn run(port, backlog, bufSize, callback)
 
   sync()
+
+proc hash(str: string): Hash =
+  var h = 0
+  
+  for v in str:
+    h = h !& v.int
+
+  result = !$h
+
+proc create(body, stmt: NimNode, i: int) {.compileTime.} =
+  stmt.add(body[i][2])
+
+macro routes*(body: untyped): typed =
+  result = newStmtList()
+
+  var
+    methodCase = newNimNode(nnkCaseStmt)
+    methodTables = initTable[string, NimNode]()
+    caseTables = initTable[string, NimNode]()
+
+  methodCase.add(
+    newCall(
+      "getMethod",
+      ident("req")
+    )
+  )
+
+  for i in 0 ..< body.len:
+    case body[i].kind
+    of nnkCommand:
+      let
+        cmdName = body[i][0].ident.`$`.normalize.toUpper()
+        cmdPath = $body[i][1]
+
+      if not methodTables.hasKey(cmdName):
+        methodTables[cmdName] = newNimNode(nnkOfBranch)
+
+        methodTables[cmdName].add(newLit(cmdName))
+
+      if not caseTables.hasKey(cmdName):
+        caseTables[cmdName] = newNimNode(nnkCaseStmt)
+      
+        caseTables[cmdName].add(
+          newCall(
+            "getPath",
+            ident("req")
+          )
+        )
+
+      var stmt = newNimNode(nnkOfBranch)
+      stmt.add(newLit(cmdPath))
+      create(body, stmt, i)
+      caseTables[cmdName].add(stmt)
+    else:
+      discard
+
+  var elseMethod = newNimNode(nnkElse)
+
+  elseMethod.add(
+    newStmtList(
+      newNimNode(nnkCommand).add(
+        newIdentNode("asyncCheck"),
+        newCall(
+          "notFound",
+          ident("res")
+        )
+      )
+    )
+  )
+
+  for k, v in caseTables.pairs:
+    v.add(elseMethod)
+    methodTables[k].add(v)
+
+  for k, v in methodTables.pairs:
+    methodCase.add(v)
+
+  methodCase.add(elseMethod)
+
+  result.add(methodCase)
+
+  dumpAstGen:
+    mofuw.callback = proc(req: mofuwReq, res: mofuwRes) {.async.} =
+      await call("test")
