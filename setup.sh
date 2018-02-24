255f2a325ba0a0b8fb150fea60370a7e01bf0aca
2vg <m0fqn090310@gmail.com>
2018-02-24 11:21:49 +0900

    edit: update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,6 @@
 
 set -e -u -o pipefail
 
-wget https://dist.libuv.org/dist/v1.19.1/libuv-v1.19.1.tar.gz
-
-tar xf libuv-v1.19.1.tar.gz
-
-cd libuv-v1.19.1
-
-sh autogen.sh
-
-./configure
-
-make
-
-make install
-
-cd ../
-
 git clone -b devel https://github.com/nim-lang/Nim.git nim
 
 cd nim
@@ -33,7 +17,5 @@ cd ../
 bin/nim c koch
 
 ./koch boot -d:release
-
-./koch nimble
 
 ./koch tools
