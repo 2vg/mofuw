51f3d9dd9588054b8acb30e47664dc435752f70b
2vg <m0fqn090310@gmail.com>
2018-02-24 11:21:20 +0900

    edit: update mofuwStaticServe

--- middleware/staticServe/mofuwStaticServe.nim
+++ middleware/staticServe/mofuwStaticServe.nim
@@ -1,14 +1,13 @@
-import mofuw, os, mimetypes, uri
+import mofuw, os, ospaths, mimetypes, uri, asyncfile
 
 proc reverse(s: var string) =
   for i in 0 .. s.high div 2:
     swap(s[i], s[s.high - i])
 
-proc serveStatic*(req: mofuwReq, res: mofuwRes, rootPath: string): bool =
+proc serveStatic*(req: mofuwReq, res: mofuwRes, rootPath: string): Future[bool] {.async.} =
   var
     state = 0
     reqPath = getPath(req)
-    fileName = ""
     fileExt = ""
     filePath = rootPath
     file: string
@@ -16,7 +15,7 @@ proc serveStatic*(req: mofuwReq, res: mofuwRes, rootPath: string): bool =
   for k, v in reqPath:
     if v == '.':
       if reqPath[k+1] == '.':
-        res.mofuw_send(badRequest())
+        await res.mofuw_send(badRequest())
         return true
 
   if filePath[^1] != '/':
@@ -35,45 +34,43 @@ proc serveStatic*(req: mofuwReq, res: mofuwRes, rootPath: string): bool =
 
       reqPath.add("/")
 
-      res.mofuw_send(redirectTo(
+      await res.mofuw_send(redirectTo(
         "http://" / host / reqPath
       ))
 
       return true
     if fileExists(filePath):
-      file = readFile(filePath)
+      let f = openAsync(filePath, fmRead)
+      file = await f.readAll()
+      close(f)
     else:
       return false
   else:
     filePath.add("index.html")
     if fileExists(filePath):
-      file = readFile(filePath)
+      let f = openAsync(filePath, fmRead)
+      file = await f.readAll()
+      close(f)
     else:
       return false
 
-  state = 0
-  reverse(filePath)
+  let (_, _, ext) = splitFile(filePath)
 
-  for k, v in filePath:
-    case v
-    of '/':
-      fileName.add(filePath[state .. k-1])
-      if fileName != "": reverse(fileName)
-      break
-    of '.':
-      fileExt.add(filePath[0 .. k-1])
-      reverse(fileExt)
-      state = k + 1
-    else:
-      discard
+  shallowcopy(fileExt, ext[1 .. ^1])
 
-  let
-    mime = newMimetypes()
+  if ext == "":
+    await res.mofuw_send(makeResp(
+      HTTP200,
+      "text/plain",
+      file
+    ))
+  else:
+    let mime = newMimetypes()
 
-  res.mofuw_send(makeResp(
-    HTTP200,
-    mime.getMimetype(fileExt, default = "application/octet-stream"),
-    file
-  ))
+    await res.mofuw_send(makeResp(
+      HTTP200,
+      mime.getMimetype(ext[1 .. ^1], default = "application/octet-stream"),
+      file
+    ))
 
   return true
