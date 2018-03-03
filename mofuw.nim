03af88ea1cbdb0c26a6a9ec7b7d206e40bbda3af
2vg@github.com <mail@ni-vg.com>
2018-03-03 21:08:36 +0900

    remove: remove non need import

--- mofuw.nim
+++ mofuw.nim
@@ -11,9 +11,7 @@ import
   nativesockets
 
 when defined(windows):
-  from winlean import TCP_NODELAY, WSAEWOULDBLOCK
-
-  const EAGAIN = WSAEWOULDBLOCK
+  from winlean import TCP_NODELAY
 else:
   from posix import TCP_NODELAY, EAGAIN
 
