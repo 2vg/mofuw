af7a891e686747bba22de00e09f30583ed5c2a78
2vg <m0fqn090310@gmail.com>
2018-05-10 07:20:47 +0900

    fix techempower tests

--- tests/techempower/techempower.nim
+++ tests/techempower/techempower.nim
@@ -1,11 +1,9 @@
 import mofuw
 
 proc h(req: mofuwReq, res: mofuwRes) {.async.} =
-  if getPath(req) == "/plaintext":
-    asyncCheck res.cacheResp("/plaintext",
-      HTTP200, "text/plain", "Hello, World!"
-    )
+  if req.getPath == "/plaintext":
+    mofuwResp(HTTP200, "text/plain", "Hello, World!")
   else:
     mofuwResp(HTTP404, "text/plain", "NOT FOUND")
 
-h.mofuwRun(port = 8080, bufSize = 512)
+h.mofuwRun(port = 8080)
