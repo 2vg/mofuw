725db91213646523f3c56f0a8e6cb1b65515094c
momf <mail@ni-vg.com>
2018-10-02 17:54:20 +0900

    fix test

--- tests/helloworld/minimal.nim
+++ tests/helloworld/minimal.nim
@@ -1,28 +1,8 @@
 import ../../src/mofuw
-import strutils, tables
-
-proc mytest*(x:string):string=
-  result="hello " & x
-  
-type
-  appHandler* =ref object of RootObj
-    proctable* :Table[string,proc(x:string):string]
-  
-proc initProcTable* (this:appHandler):void {.inline.}=
-  this.proctable["mytest"]=mytest
-  
-proc call* (this:appHandler,ctx:MofuwCtx):void {.inline.}=
-  var webresult:string=this.proctable[ctx.body]("world")
-  mofuwResp(HTTP200,"text/plain",webresult)
-
-var myapp {.threadvar.}: appHandler
-
-myapp = appHandler()
-myapp.initProcTable()
 
 routes:
-  post "/test":
-    myapp.call(ctx)
+  get "/":
+    mofuwOK("hello, world!")
 
 newServeCtx(
   port = 8080,
